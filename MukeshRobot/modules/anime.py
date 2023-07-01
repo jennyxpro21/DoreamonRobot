a6001664911eda5ed4688ba6cd3f4ad49cec8225
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2023-07-01 22:14:38 +0530

    Update anime.py

--- MukeshRobot/modules/anime.py
+++ MukeshRobot/modules/anime.py
@@ -139,7 +139,7 @@ dispatcher.add_handler(QUOTE_CHANGE)
 dispatcher.add_handler(ANIMEQUOTES_HANDLER)
 dispatcher.add_handler(QUOTES_HANDLER)
 
-__mod_name__ = "ǫᴜᴏᴛᴇs"
+__mod_name__ = "𝐐υσƚҽ𝐒"
 __help__ = """
 /quote :- ᴡʀɪᴛᴇ ǫᴜᴏᴛᴇs
 /animequotes :- ᴡʀɪᴛᴇ ᴀɴɪᴍᴇǫᴜᴏᴛᴇs
