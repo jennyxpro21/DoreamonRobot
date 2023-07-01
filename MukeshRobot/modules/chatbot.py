536e4a001b0786f5a1794a244ac8660bb0ffd253
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2023-07-02 00:02:48 +0530

    Update chatbot.py

--- MukeshRobot/modules/chatbot.py
+++ MukeshRobot/modules/chatbot.py
@@ -143,7 +143,7 @@ __help__ = f"""
  »  /ᴄʜᴀᴛʙᴏᴛ *:* sʜᴏᴡs ᴄʜᴀᴛʙᴏᴛ ᴄᴏɴᴛʀᴏʟ ᴘᴀɴᴇʟ
 """
 
-__mod_name__ = "Cʜᴀᴛʙᴏᴛ"
+__mod_name__ = "💘𝐂ԋαƚႦσ𝐓💘"
 
 
 CHATBOTK_HANDLER = CommandHandler("chatbot", fallen, run_async=True)
